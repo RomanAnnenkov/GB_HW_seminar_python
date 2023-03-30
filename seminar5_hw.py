94b4a1f43706af0a5e3c7539a70002a1bef887c0
Roman Annenkov <roman.annenkov@gmail.com>
2023-03-30 14:10:35 +0300

    fix algorithms, this solution is more unuversal

--- seminar5_hw.py
+++ seminar5_hw.py
@@ -8,13 +8,11 @@
 def group_words_with_same_letters(words: list):
     groups = {}
     set_list = []
-    for i in words:
-        tmp = set(i)
-        if tmp not in set_list:
-            set_list.append(tmp)
-            groups[set_list.index(set(i))] = []
-    for i in words:
-        groups[set_list.index(set(i))].append(i)
+    for word in words:
+        if (frozenset(word), len(word)) not in groups:
+            groups[(frozenset(word), len(word))] = [word]
+        else:
+            groups[(frozenset(word), len(word))].append(word)
     groups = list(groups.values())
     return groups
 
@@ -36,22 +34,22 @@ def compress_str(string):
     validation = [chr(i) for i in range(ord("A"), ord("Z") + 1)]
     if not set(string).issubset(validation):
         return "невалидная строка"
-    result = ""
+    result = []
     count = 0
     last_index = len(string) - 1
     for i in range(len(string)):
         current_char = string[i]
         if i != last_index and current_char != string[i + 1]:
-            result += current_char
+            result.append(current_char)
             if count != 0:
-                result += str(count + 1)
+                result.append(count + 1)
                 count = 0
         else:
             count += 1
             if i == last_index:
-                result += current_char + str(count)
+                result.append(f"{current_char}{count}")
     return result
 
 
 input_str = "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-print(compress_str(input_str))
+print(*compress_str(input_str), sep="")
